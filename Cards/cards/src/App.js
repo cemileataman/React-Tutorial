f63682005ce9e768f3bbfbe672428c25f99d34f7
Cemile Ataman <cemileataman444@gmail.com>
2024-04-30 17:54:25 +0300

    cards folder added

---
+++ Cards/cards/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Course } from './Course';
+
+function App() {
+  return (
+    <div className="App">
+   <Course/>
+   <Course/>
+   <Course/>
+   <Course/>
+    </div>
+  );
+}
+
+export default App;
