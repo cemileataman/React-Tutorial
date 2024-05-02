489f37f81100622a90d930ad6d45baa345ecaf97
Cemile Ataman <cemileataman444@gmail.com>
2024-05-02 16:13:37 +0300

    css part has been edited

--- Cards/cards/src/App.js
+++ Cards/cards/src/App.js
@@ -1,48 +1,75 @@
 /* kapsayacı component appjs child component ise course.js */
+import "bulma/css/bulma.css";
 import "./App.css";
-import Course from './Course';
-import Angular from './images/angular.jpg';
-import Boostrap from './images/bootstrap5.png';
-import Csharp from './images/ccsharp.png';
-import Kompleweb from './images/kompleweb.jpg';
+import Course from "./Course";
+import Angular from "./images/angular.jpg";
+import Boostrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import Kompleweb from "./images/kompleweb.jpg";
 /* props olarak course componentlere göndereceğiz */
 
 function App() {
   return (
     <div className="App">
-      {/* <img src={Angular} alt=""/> */}
-      <Course /* Course component */
-        image={Angular} /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
-        title="Angular"/* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
-        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course /* Course component */
+                image={
+                  Angular
+                } /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
+                title="Angular" /* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
+                description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
     Ratione hic laudantium dicta nemo, eum totam. 
-   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
-      />
-       <Course
-       image={Boostrap}
-        title="Boostrap 5"
-        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Expedita corrupti debitis iusto enim ullam non?" /* key,value değeri bunlar birer objedir. */
+              />
+            </div>
+            <div className="column">
+              <Course /* Course component */
+                image={
+                  Boostrap
+                } /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
+                title="Boostrap 5" /* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
+                description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
     Ratione hic laudantium dicta nemo, eum totam. 
-   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
-      />
-       <Course
-       image={Csharp}
-        title="Komple Web"
-        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Expedita corrupti debitis iusto enim ullam non?" /* key,value değeri bunlar birer objedir. */
+              />
+            </div>
+            <div className="column">
+              <Course /* Course component */
+                image={
+                  Csharp
+                } /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
+                title="Komple Web" /* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
+                description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
     Ratione hic laudantium dicta nemo, eum totam. 
-   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
-      />
-       <Course
-       image={Kompleweb}
-        title="FrontEnd"
-        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+   Expedita corrupti debitis iusto enim ullam non?" /* key,value değeri bunlar birer objedir. */
+              />
+            </div>
+            <div className="column">
+              <Course /* Course component */
+                image={
+                  Kompleweb
+                } /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
+                title="FrontEnd" /* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
+                description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
     Ratione hic laudantium dicta nemo, eum totam. 
-   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
-      />
+   Expedita corrupti debitis iusto enim ullam non?" /* key,value değeri bunlar birer objedir. */
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- Cards/cards/src/Course.js
+++ Cards/cards/src/Course.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-function Course({image,title,description}) {
-//   console.log(props);
-//   const title=props.title;
-//   const description =props.description;
-// const {title,description}=props;/* Yukarıda yazdığımız iki satır kod ile bu kod aynı */
+function Course({ image, title, description }) {
   return (
-    <div>
-      <img src={image} alt=""/>
-      <div>{title}</div>
-      <div>{description}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="Kurslarım" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
