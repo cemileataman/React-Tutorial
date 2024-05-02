bbc9613455eb62dce5811ed71db047c57496e15b
Cemile Ataman <cemileataman444@gmail.com>
2024-05-02 10:29:23 +0300

    use of props

--- Cards/cards/src/App.js
+++ Cards/cards/src/App.js
@@ -1,13 +1,38 @@
-import './App.css';
-import { Course } from './Course';
+/* kapsayacı component appjs child component ise course.js */
+import "./App.css";
+import Course from './Course';
 
 function App() {
   return (
     <div className="App">
-   <Course/>
-   <Course/>
-   <Course/>
-   <Course/>
+      <Course /* Course component */
+        title="Angular"
+        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quam accusantium corporis modi dicta minus voluptatibus possimus? 
+    Ratione hic laudantium dicta nemo, eum totam. 
+   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
+      />
+       <Course
+        title="Boostrap 5"
+        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quam accusantium corporis modi dicta minus voluptatibus possimus? 
+    Ratione hic laudantium dicta nemo, eum totam. 
+   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
+      />
+       <Course
+        title="Komple Web"
+        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quam accusantium corporis modi dicta minus voluptatibus possimus? 
+    Ratione hic laudantium dicta nemo, eum totam. 
+   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
+      />
+       <Course
+        title="FrontEnd"
+        description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quam accusantium corporis modi dicta minus voluptatibus possimus? 
+    Ratione hic laudantium dicta nemo, eum totam. 
+   Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
+      />
     </div>
   );
 }
--- Cards/cards/src/Course.js
+++ Cards/cards/src/Course.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function Course() {
-    return <div>Course div</div>;
+function Course({title,description}) {
+//   console.log(props);
+//   const title=props.title;
+//   const description =props.description;
+// const {title,description}=props;/* Yukarıda yazdığımız iki satır kod ile bu kod aynı */
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{description}</div>
+    </div>
+  );
 }
 
 export default Course;
+/* props ismini nasıl verilmişse o şekilde yazılmalı */
