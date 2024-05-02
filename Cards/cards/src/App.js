533ee8315a9c068d1467ff356042e564d243466f
Cemile Ataman <cemileataman444@gmail.com>
2024-05-02 12:57:21 +0300

    Send Images as Props

--- Cards/cards/src/App.js
+++ Cards/cards/src/App.js
@@ -1,18 +1,26 @@
 /* kapsayacı component appjs child component ise course.js */
 import "./App.css";
 import Course from './Course';
+import Angular from './images/angular.jpg';
+import Boostrap from './images/bootstrap5.png';
+import Csharp from './images/ccsharp.png';
+import Kompleweb from './images/kompleweb.jpg';
+/* props olarak course componentlere göndereceğiz */
 
 function App() {
   return (
     <div className="App">
+      {/* <img src={Angular} alt=""/> */}
       <Course /* Course component */
-        title="Angular"
+        image={Angular} /* Yukarıda tanımlanmış bir değişkeni importu vermek isteğimde jsx formatında süslü parantezler kulllanır. . */
+        title="Angular"/* Yukarıda tanımlanmamışsa bu şekilde tırnak  */
         description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
     Ratione hic laudantium dicta nemo, eum totam. 
    Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
       />
        <Course
+       image={Boostrap}
         title="Boostrap 5"
         description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
@@ -20,6 +28,7 @@ function App() {
    Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
       />
        <Course
+       image={Csharp}
         title="Komple Web"
         description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
@@ -27,6 +36,7 @@ function App() {
    Expedita corrupti debitis iusto enim ullam non?"/* key,value değeri bunlar birer objedir. */
       />
        <Course
+       image={Kompleweb}
         title="FrontEnd"
         description="  Lorem ipsum dolor sit amet consectetur adipisicing elit.
     Quam accusantium corporis modi dicta minus voluptatibus possimus? 
