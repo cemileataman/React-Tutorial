2976ea5dcbb4ef284c45312f445265e05c8454a0
Cemile Ataman <cemileataman444@gmail.com>
2024-05-06 17:48:17 +0300

    to-do list project

---
+++ tasks/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const createTask = (title, taskDesc) => {
+    const createdTasks = [
+      ...tasks,
+      {
+        id: Math.round(Math.random() * 999999),
+        title,
+        taskDesc,
+      },
+    ];
+    setTasks(createdTasks);
+  };
+  
+
+  return (
+    <div className="App">
+      <TaskCreate onCreate={createTask} />
+      <h1>Görevler</h1>
+      <TaskList tasks={tasks} />
+    </div>
+  );
+}
+
+export default App;
